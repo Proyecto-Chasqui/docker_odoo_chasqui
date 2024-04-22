d39cb6c6ddca98ff8c1e062ed77f4c91e12046f3
barct <hidalgofernandojavier@gmail.com>
2024-04-22 21:47:44 +0000

    fix submodules branch

--- odoo16/partials_addons/odoo4chasqui/__manifest__.py
+++ odoo16/partials_addons/odoo4chasqui/__manifest__.py
@@ -13,7 +13,7 @@
         'l10n_ar_afipws', 'l10n_ar_afipws_fe', 'l10n_ar_bank',
         'l10n_ar_account_withholding','l10n_ar_bank','l10n_ar_purchase','l10n_ar_purchase_stock', 'l10n_ar_ux',
         'account_mass_reconcile','account_move_base_import','account_move_line_reconcile_manual','account_move_reconcile_forbid_cancel',,'account_reconcile_oca','account_statement_base','base_transaction_id',
-        'account_statement_import_base','account_statement_import_camt','account_statement_import_camt54','account_statement_import_file','account_statement_import_ofx','account_statement_import_online','account_statement_import_online_paypal','account_statement_import_online_ponto','account_statement_import_sheet_file',
+        'account_statement_import_base','account_statement_import_camt','account_statement_import_camt54','account_statement_import_ofx','account_statement_import_online','account_statement_import_online_paypal','account_statement_import_online_ponto','account_statement_import_sheet_file',
         'odoo_chasqui_integration',
         'l10n_ar_report_fe','l10n_ar_fe_qr'
         ], 
